94d1859d8a8a4ec5bf5ba96d18ee976e4cd7deba
Thomas Müller <git@tom94.net>
2026-02-21 16:13:07 +0100

    fix(texture_gl): GL 4.2 requirement in mip level allocation

--- src/texture_gl.cpp
+++ src/texture_gl.cpp
@@ -162,6 +162,30 @@ void Texture::upload(const uint8_t *data) {
         if (data)
             CHK(glPixelStorei(GL_UNPACK_ALIGNMENT, 1));
 
+        const auto preallocate_mip_levels = [&]() {
+            // Pre-allocate mip levels to prevent a pipeline stall upon future glGenerateMipmap() calls
+            if (m_min_interpolation_mode == InterpolationMode::Trilinear ||
+                m_mag_interpolation_mode == InterpolationMode::Trilinear) {
+                GLint prev_pbo = 0;
+                CHK(glGetIntegerv(GL_PIXEL_UNPACK_BUFFER_BINDING, &prev_pbo));
+                if (prev_pbo != 0)
+                    CHK(glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0));
+
+                int mip_levels = 1 + (int) floor(log2((double) std::max(m_size.x(), m_size.y())));
+                CHK(glTexParameteri(tex_mode, GL_TEXTURE_MAX_LEVEL, mip_levels - 1));
+
+                for (int level = 0; level < mip_levels; ++level) {
+                    GLsizei w = std::max(1, (GLsizei) m_size.x() >> level);
+                    GLsizei h = std::max(1, (GLsizei) m_size.y() >> level);
+                    CHK(glTexImage2D(tex_mode, level, internal_format_gl, w, h, 0,
+                                     pixel_format_gl, component_format_gl, nullptr));
+                }
+
+                if (prev_pbo != 0)
+                    CHK(glBindBuffer(GL_PIXEL_UNPACK_BUFFER, prev_pbo));
+            }
+        };
+
 #if defined(NANOGUI_USE_OPENGL)
         if (data) {
             CHK(glPixelStorei(GL_UNPACK_ROW_LENGTH, 0));
@@ -170,31 +194,17 @@ void Texture::upload(const uint8_t *data) {
         }
 
         if (m_samples == 1) {
-            if (m_min_interpolation_mode == InterpolationMode::Trilinear ||
-                m_mag_interpolation_mode == InterpolationMode::Trilinear) {
-                int mip_levels = 1 + (int) floor(log2((double) std::max(m_size.x(), m_size.y())));
-                CHK(glTexStorage2D(tex_mode, mip_levels, internal_format_gl, m_size.x(), m_size.y()));
-                CHK(glTexSubImage2D(tex_mode, 0, 0, 0, (GLsizei) m_size.x(), (GLsizei) m_size.y(),
-                                    pixel_format_gl, component_format_gl, data));
-            } else
-                CHK(glTexImage2D(tex_mode, 0, internal_format_gl, (GLsizei) m_size.x(),
-                                 (GLsizei) m_size.y(), 0, pixel_format_gl, component_format_gl, data));
+            preallocate_mip_levels();
+            CHK(glTexImage2D(tex_mode, 0, internal_format_gl, (GLsizei) m_size.x(),
+                             (GLsizei) m_size.y(), 0, pixel_format_gl, component_format_gl, data));
         }
         else
             CHK(glTexImage2DMultisample(tex_mode, m_samples, internal_format_gl,
                                         (GLsizei) m_size.x(), (GLsizei) m_size.y(), false));
 #else
-#if defined(NANOGUI_USE_GLES) && NANOGUI_GLES_VERSION >= 3
-        if (m_min_interpolation_mode == InterpolationMode::Trilinear ||
-            m_mag_interpolation_mode == InterpolationMode::Trilinear) {
-            int mip_levels = 1 + (int) floor(log2((double) std::max(m_size.x(), m_size.y())));
-            CHK(glTexStorage2D(tex_mode, mip_levels, internal_format_gl, m_size.x(), m_size.y()));
-            CHK(glTexSubImage2D(tex_mode, 0, 0, 0, (GLsizei) m_size.x(), (GLsizei) m_size.y(),
-                                pixel_format_gl, component_format_gl, data));
-        } else
-#endif
-            CHK(glTexImage2D(tex_mode, 0, internal_format_gl, (GLsizei) m_size.x(),
-                             (GLsizei) m_size.y(), 0, pixel_format_gl, component_format_gl, data));
+        preallocate_mip_levels();
+        CHK(glTexImage2D(tex_mode, 0, internal_format_gl, (GLsizei) m_size.x(),
+                         (GLsizei) m_size.y(), 0, pixel_format_gl, component_format_gl, data));
 #endif
 
         if (!m_mipmap_manual && (m_min_interpolation_mode == InterpolationMode::Trilinear ||
